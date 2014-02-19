ee74826610e3f8952d59df1e3299825f55d3eb37
Lukas Boehm <suluke93@gmail.com>
2014-02-19 10:57:22 +0100

    Implement DesktopContentValues and clean up BackendHelper

--- src/de/croggle/backends/BackendHelper.java
+++ src/de/croggle/backends/BackendHelper.java
@@ -1,9 +1,5 @@
 package de.croggle.backends;
 
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-
-import com.badlogic.gdx.Application;
 import com.badlogic.gdx.Gdx;
 
 import de.croggle.backends.desktop.DesktopContentValues;
@@ -24,67 +20,11 @@ public class BackendHelper {
 	private BackendHelper() {
 	}
 
-	public static Object getWakeLock() {
-		Application app = Gdx.app;
-		Method getWakeLock = null;
-		try {
-			getWakeLock = app.getClass().getMethod("getWakeLock");
-		} catch (NoSuchMethodException e) {
-		}
-		if (getWakeLock != null) {
-			try {
-				return getWakeLock.invoke(app);
-			} catch (IllegalAccessException e) {
-				return null;
-			} catch (InvocationTargetException e) {
-				return null;
-			} catch (IllegalArgumentException e) {
-				return null;
-			}
-		} else {
-			return null;
-		}
-	}
-
 	public static boolean acquireWakeLock() {
-		Object wakeLock = getWakeLock();
-		if (wakeLock != null) {
-			Method acquire = null;
-			try {
-				acquire = wakeLock.getClass().getMethod("acquire");
-			} catch (NoSuchMethodException e) {
-			}
-			if (acquire != null) {
-				try {
-					acquire.invoke(wakeLock);
-					return true;
-				} catch (IllegalAccessException e) {
-				} catch (IllegalArgumentException e) {
-				} catch (InvocationTargetException e) {
-				}
-			}
-		}
 		return false;
 	}
 
 	public static boolean releaseWakeLock() {
-		Object wakeLock = getWakeLock();
-		if (wakeLock != null) {
-			Method release = null;
-			try {
-				release = wakeLock.getClass().getMethod("release");
-			} catch (NoSuchMethodException e) {
-			}
-			if (release != null) {
-				try {
-					release.invoke(wakeLock);
-					return true;
-				} catch (IllegalAccessException e) {
-				} catch (IllegalArgumentException e) {
-				} catch (InvocationTargetException e) {
-				}
-			}
-		}
 		return false;
 	}
 
--- src/de/croggle/backends/desktop/DesktopContentValues.java
+++ src/de/croggle/backends/desktop/DesktopContentValues.java
@@ -1,37 +1,40 @@
 package de.croggle.backends.desktop;
 
+import java.util.HashMap;
+
 import de.croggle.backends.sqlite.ContentValues;
 
 public class DesktopContentValues implements ContentValues {
 
+	private final HashMap<String, Object> values;
+
+	public DesktopContentValues() {
+		values = new HashMap<String, Object>();
+	}
+
 	@Override
 	public void put(String key, float value) {
-		// TODO Auto-generated method stub
-
+		values.put(key, value);
 	}
 
 	@Override
 	public void put(String key, String value) {
-		// TODO Auto-generated method stub
-
+		values.put(key, value);
 	}
 
 	@Override
 	public void put(String key, int value) {
-		// TODO Auto-generated method stub
-
+		values.put(key, value);
 	}
 
 	@Override
 	public void put(String key, boolean value) {
-		// TODO Auto-generated method stub
-
+		values.put(key, value);
 	}
 
 	@Override
 	public Object get() {
-		// TODO Auto-generated method stub
-		return null;
+		return values;
 	}
 
 }
