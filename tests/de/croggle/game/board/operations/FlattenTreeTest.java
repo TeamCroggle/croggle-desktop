9be7a129d2acd9ca372b1ff6cc05c44489a8ad03
Tobias Hornberger <saibot1207@googlemail.com>
2014-03-19 19:51:42 +0100

    Improve testCoverage via FlattenTreeTest

--- tests/de/croggle/game/board/operations/FlattenTreeTest.java
+++ tests/de/croggle/game/board/operations/FlattenTreeTest.java
@@ -7,6 +7,7 @@ import de.croggle.game.board.Board;
 import de.croggle.game.board.BoardObject;
 import de.croggle.game.board.ColoredAlligator;
 import de.croggle.game.board.Egg;
+import de.croggle.game.board.InternalBoardObject;
 import junit.framework.TestCase;
 
 public class FlattenTreeTest extends TestCase {
@@ -27,5 +28,29 @@ public class FlattenTreeTest extends TestCase {
 		assertTrue(list.contains(a));
 		assertTrue(list.contains(e1));
 		assertTrue(list.contains(e2));
+		
+		List<InternalBoardObject> internalList = FlattenTree.toList(a);
+		
+		assertEquals(internalList.size(), 2);
+		assertTrue(internalList.contains(a));
+		assertTrue(internalList.contains(e1));
+		assertFalse(internalList.contains(e2));
+		
+		BoardObject boardObjectsArray[] = FlattenTree.toArray(b);
+		
+		assertEquals(4, boardObjectsArray.length); 
+		assertTrue(boardObjectsArray[0].equals(b));
+		assertTrue(boardObjectsArray[1].equals(a));
+		assertTrue(boardObjectsArray[2].equals(e1));
+		assertTrue(boardObjectsArray[3].equals(e2));
+		
+		InternalBoardObject internalBoardObjectsArray[] = FlattenTree.toArray(a);
+		
+		assertEquals(internalBoardObjectsArray.length, 2);
+		assertTrue(internalBoardObjectsArray[0].equals(a));
+		assertTrue(internalBoardObjectsArray[1].equals(e1));
+		
+		
+		
 	}
 }
