2df42d91579d6c6898496a4c981ed5a2234dcc51
Tobias Hornberger <saibot1207@googlemail.com>
2014-03-22 17:57:21 +0100

    Improve Test Coverage in AlligatorToJsonTest

--- tests/de/croggle/util/convert/AlligatorToJsonTest.java
+++ tests/de/croggle/util/convert/AlligatorToJsonTest.java
@@ -2,6 +2,7 @@ package de.croggle.util.convert;
 
 import junit.framework.TestCase;
 import de.croggle.game.Color;
+import de.croggle.game.board.AgedAlligator;
 import de.croggle.game.board.Board;
 import de.croggle.game.board.ColoredAlligator;
 import de.croggle.game.board.Egg;
@@ -20,6 +21,8 @@ public class AlligatorToJsonTest extends TestCase {
 		a.addChild(e1);
 		Egg e2 = new Egg(true, true, new Color(1), true);
 		b.addChild(e2);
+		Egg e3 = new Egg(true, true, new Color(1), true);
+		b.addChild(e3);
 
 		String expected = "{\n\t\"" + "families\" : [\n" + "\t\t{\n"
 				+ "\t\t\t\"type\" : \"colored alligator\",\n"
@@ -36,7 +39,48 @@ public class AlligatorToJsonTest extends TestCase {
 				+ "\t\t\t\"type\" : \"egg\",\n" + "\t\t\t\"color\" : 1,\n"
 				+ "\t\t\t\"movable\" : true,\n"
 				+ "\t\t\t\"removable\" : true,\n"
-				+ "\t\t\t\"recolorable\" : true\n" + "\t\t}\n\t]\n" + "}\n";
+				+ "\t\t\t\"recolorable\" : true\n"
+				+ "\t\t},\n" + "\t\t{\n"
+				+ "\t\t\t\"type\" : \"egg\",\n" + "\t\t\t\"color\" : 1,\n"
+				+ "\t\t\t\"movable\" : true,\n"
+				+ "\t\t\t\"removable\" : true,\n"
+				+ "\t\t\t\"recolorable\" : true\n"
+				+ "\t\t}\n\t]\n" + "}\n";
+		assertEquals(expected, AlligatorToJson.convert(b));
+	}
+	
+	public void testSimpleManual2() {
+		Board b = new Board();
+		AgedAlligator a = new AgedAlligator(true, true);
+		b.addChild(a);
+		Egg e1 = new Egg(true, true, new Color(0), true);
+		a.addChild(e1);
+		Egg e2 = new Egg(true, true, new Color(1), true);
+		b.addChild(e2);
+		Egg e3 = new Egg(true, true, new Color(1), true);
+		b.addChild(e3);
+
+		String expected = "{\n\t\"" + "families\" : [\n" + "\t\t{\n"
+				+ "\t\t\t\"type\" : \"aged alligator\",\n"
+				+ "\t\t\t\"movable\" : true,\n"
+				+ "\t\t\t\"removable\" : true,\n"
+				+ "\t\t\t\"children\" : [\n" + "\t\t\t\t{\n"
+				+ "\t\t\t\t\t\"type\" : \"egg\",\n"
+				+ "\t\t\t\t\t\"color\" : 0,\n"
+				+ "\t\t\t\t\t\"movable\" : true,\n"
+				+ "\t\t\t\t\t\"removable\" : true,\n"
+				+ "\t\t\t\t\t\"recolorable\" : true\n" + "\t\t\t\t}\n"
+				+ "\t\t\t]\n" + "\t\t},\n" + "\t\t{\n"
+				+ "\t\t\t\"type\" : \"egg\",\n" + "\t\t\t\"color\" : 1,\n"
+				+ "\t\t\t\"movable\" : true,\n"
+				+ "\t\t\t\"removable\" : true,\n"
+				+ "\t\t\t\"recolorable\" : true\n"
+				+ "\t\t},\n" + "\t\t{\n"
+				+ "\t\t\t\"type\" : \"egg\",\n" + "\t\t\t\"color\" : 1,\n"
+				+ "\t\t\t\"movable\" : true,\n"
+				+ "\t\t\t\"removable\" : true,\n"
+				+ "\t\t\t\"recolorable\" : true\n"
+				+ "\t\t}\n\t]\n" + "}\n";
 		assertEquals(expected, AlligatorToJson.convert(b));
 	}
 
