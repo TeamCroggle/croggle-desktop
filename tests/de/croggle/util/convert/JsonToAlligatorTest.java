94c3f881199e7c773d7a8ef79adf616a2b9cd12d
Tobias Hornberger <saibot1207@googlemail.com>
2014-03-24 14:20:37 +0100

    Improve testCoverage in JsonToAlligatorTest

--- tests/de/croggle/util/convert/JsonToAlligatorTest.java
+++ tests/de/croggle/util/convert/JsonToAlligatorTest.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.utils.JsonReader;
 import com.badlogic.gdx.utils.JsonValue;
 
 import de.croggle.game.Color;
+import de.croggle.game.board.AgedAlligator;
 import de.croggle.game.board.Board;
 import de.croggle.game.board.BoardObject;
 import de.croggle.game.board.ColoredAlligator;
@@ -74,8 +75,56 @@ public class JsonToAlligatorTest extends TestCase {
 	
 	public void testInternalConvert() {
 		Egg e1 = new Egg(true, true, new Color(0), true);
+		ColoredAlligator c2 = new ColoredAlligator(true, true, new Color(0), true);
+		c2.addChild(e1);
 		String testString = AlligatorToJson.convert(e1);
+		JsonValue testJson = new JsonValue(testString);
 		assertTrue(e1.match(JsonToAlligator.convertInternalBoardObject(testString)));
+	}
+	
+	public void testMoreComplexManual() {
+		Board b = new Board();
+		ColoredAlligator a = new ColoredAlligator(true, true, new Color(0),
+				true);
+		b.addChild(a);
+		Egg e1 = new Egg(true, true, new Color(0), true);
+		a.addChild(e1);
+		Egg e2 = new Egg(true, true, new Color(1), true);
+		a.addChild(e2);
+		Egg e3 = new Egg(true, true, new Color(1), true);
+		b.addChild(e3);
+		AgedAlligator a2 = new AgedAlligator(true, true);
+		b.addChild(a2);
+		ColoredAlligator a3 = new ColoredAlligator(true, true, new Color(2),
+				true);
+		ColoredAlligator a4 = new ColoredAlligator(true, true, new Color(4),
+				true);
+		a2.addChild(a3);
+		a2.addChild(a4);
+		Egg e4 = new Egg(true, true, new Color(2), true);
+		Egg e5 = new Egg(true, true, new Color(6), true);
+		Egg e6 = new Egg(true, true, new Color(4), true);
+		a3.addChild(e4);
+		a3.addChild(e5);
+		a4.addChild(e6);
+		ColoredAlligator a5 = new ColoredAlligator(true, true, new Color(4),
+				true);
+		a4.addChild(a5);
+		AgedAlligator a6 = new AgedAlligator(true, true);
+		b.addChild(a6);
+		
+		String testString = AlligatorToJson.convert(b);
+		BoardObject bTest = JsonToAlligator.convert(testString);
+		
+		assertTrue(b.match(bTest));
+	}
+	
+	public void testEmptyBoard() {
+		Board b = new Board();
+		
+		String testString = AlligatorToJson.convert(b);
+		BoardObject bTest = JsonToAlligator.convert(testString);
+		assertTrue(b.match(bTest));
 	}
 	
 	public void testExceptions() {
