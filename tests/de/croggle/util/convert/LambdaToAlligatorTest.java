c799f0fa310a4d152da031e16a7511550e5eec5d
Tobias Hornberger <saibot1207@googlemail.com>
2014-03-26 10:24:21 +0100

    Improve tests for LambdaToAlligator

--- tests/de/croggle/util/convert/LambdaToAlligatorTest.java
+++ tests/de/croggle/util/convert/LambdaToAlligatorTest.java
@@ -1,7 +1,9 @@
 package de.croggle.util.convert;
 
 import junit.framework.TestCase;
+import de.croggle.game.Color;
 import de.croggle.game.board.Board;
+import de.croggle.game.board.Egg;
 
 /**
  * 
@@ -41,7 +43,7 @@ public class LambdaToAlligatorTest extends TestCase {
 		String term1 = "λx.(x.y)";
 		String term2 = "(λx.x y";
 		String term3 = "(λx x) y";
-		String term4 = "λx.x";
+		String term4 = "λ.x";
 		String term5 = "(λx.x) y q w e r t z u i o p a s d f g h j k l c v b n m 1 2 3 4 5 6 7 8 9";
 		try {
 			LambdaToAlligator.convert(term5);
@@ -71,7 +73,14 @@ public class LambdaToAlligatorTest extends TestCase {
 		}
 		catch (IllegalArgumentException e) {
 			assertTrue(true);
-		} 
+		}
+		try {
+			LambdaToAlligator.convert(term4);
+			fail();
+		}
+		catch (IllegalArgumentException e) {
+			assertTrue(true);
+		}
 		
 		
 	}
